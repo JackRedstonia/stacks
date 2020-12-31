82c77d6f1416bf547fe2a0f9c05f1007892c6f11
JackRedstonia <jackredstonia64@gmail.com>
2021-01-01 00:41:58 +0700

    Add back Parallax layer in main.rs

--- src/bin/stacks/main.rs
+++ src/bin/stacks/main.rs
@@ -6,7 +6,7 @@ use stacks::{
         widgets::{
             layout::{ContainerSize, VContainer},
             shapes::{Rect, Throbber},
-            Font, FontStyle, LayoutDimension, LayoutSize, Text, Transform, Wrappable,
+            Font, FontStyle, LayoutDimension, LayoutSize, Parallax, Text, Transform, Wrappable,
         },
         Framework,
     },
@@ -20,61 +20,64 @@ fn main() {
         .window_title("Stacks")
         .present_mode_priority(vec![PresentMode::Immediate])
         .run(|| {
-            let root = VContainer::new(
-                vec![
-                    Rect::new(
-                        LayoutSize::min(200.0, 100.0).expand_width().expand_height(),
-                        Paint::new(Color4f::new(0.2, 0.8, 0.0, 0.3), None),
-                        false,
-                    )
-                    .boxed()
-                    .wrap(),
-                    Rect::new(
-                        LayoutSize::min(100.0, 100.0).expand_height_by(3.0),
-                        Paint::new(Color4f::new(0.7, 0.1, 0.2, 0.3), None),
-                        false,
-                    )
-                    .boxed()
-                    .wrap(),
-                    Transform::new(
+            let root = Parallax::new(
+                VContainer::new(
+                    vec![
                         Rect::new(
-                            LayoutSize::min(50.0, 100.0),
-                            Paint::new(Color4f::new(0.0, 0.0, 1.0, 1.0), None),
+                            LayoutSize::min(200.0, 100.0).expand_width().expand_height(),
+                            Paint::new(Color4f::new(0.2, 0.8, 0.0, 0.3), None),
                             false,
                         )
+                        .boxed()
                         .wrap(),
-                        Matrix::translate((20.0, 20.0)),
-                    )
-                    .boxed()
-                    .wrap(),
-                    Throbber::new(
-                        LayoutDimension::min(100.0),
-                        {
-                            let mut p = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None);
-                            p.set_stroke_width(12.0);
-                            p.set_anti_alias(true);
-                            p.set_stroke(true);
-                            p
-                        },
-                        false,
-                    )
-                    .boxed()
-                    .wrap(),
-                    Text::new(
-                        LayoutSize::min(500.0, 100.0).expand_width(),
-                        "lorem ipsum dolor sit amet.  いろはにほへと",
-                        Font::Default,
-                        FontStyle::Bold,
-                        {
-                            let mut p = Paint::new(Color4f::new(1.0, 1.0, 1.0, 1.0), None);
-                            p.set_anti_alias(true);
-                            p
-                        },
-                    )
-                    .boxed()
-                    .wrap(),
-                ],
-                ContainerSize::ZERO.expand_height().expand_width(),
+                        Rect::new(
+                            LayoutSize::min(100.0, 100.0).expand_height_by(3.0),
+                            Paint::new(Color4f::new(0.7, 0.1, 0.2, 0.3), None),
+                            false,
+                        )
+                        .boxed()
+                        .wrap(),
+                        Transform::new(
+                            Rect::new(
+                                LayoutSize::min(50.0, 100.0),
+                                Paint::new(Color4f::new(0.0, 0.0, 1.0, 1.0), None),
+                                false,
+                            )
+                            .wrap(),
+                            Matrix::translate((20.0, 20.0)),
+                        )
+                        .boxed()
+                        .wrap(),
+                        Throbber::new(
+                            LayoutDimension::min(100.0),
+                            {
+                                let mut p = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None);
+                                p.set_stroke_width(12.0);
+                                p.set_anti_alias(true);
+                                p.set_stroke(true);
+                                p
+                            },
+                            false,
+                        )
+                        .boxed()
+                        .wrap(),
+                        Text::new(
+                            LayoutSize::min(500.0, 100.0).expand_width(),
+                            "lorem ipsum dolor sit amet.  いろはにほへと",
+                            Font::Default,
+                            FontStyle::Bold,
+                            {
+                                let mut p = Paint::new(Color4f::new(1.0, 1.0, 1.0, 1.0), None);
+                                p.set_anti_alias(true);
+                                p
+                            },
+                        )
+                        .boxed()
+                        .wrap(),
+                    ],
+                    ContainerSize::ZERO.expand_height().expand_width(),
+                )
+                .wrap(),
             )
             .wrap();
             Framework::new(root)
