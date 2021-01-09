0b88441dda6c31db10de1099b7ae7a2b8dd99974
JackRedstonia <jackredstonia64@gmail.com>
2021-01-09 22:18:32 +0700

    cargo fmt

--- src/game/input.rs
+++ src/game/input.rs
@@ -93,14 +93,20 @@ impl InputState {
                     return Some(EventHandleResult::Input(InputEvent::KeyUp(*k)));
                 }
             }
-            Sdl2Event::MouseButtonDown { mouse_btn, x, y, .. } => {
+            Sdl2Event::MouseButtonDown {
+                mouse_btn, x, y, ..
+            } => {
                 if let Some(kc) = Self::mouse_button_to_index(*mouse_btn) {
                     self.mouse_buttons[kc] = true;
                     let p = Point::new(*x as _, *y as _);
-                    return Some(EventHandleResult::Input(InputEvent::MouseDown(*mouse_btn, p)));
+                    return Some(EventHandleResult::Input(InputEvent::MouseDown(
+                        *mouse_btn, p,
+                    )));
                 }
             }
-            Sdl2Event::MouseButtonUp { mouse_btn, x, y, .. } => {
+            Sdl2Event::MouseButtonUp {
+                mouse_btn, x, y, ..
+            } => {
                 if let Some(kc) = Self::mouse_button_to_index(*mouse_btn) {
                     self.mouse_buttons[kc] = false;
                     let p = Point::new(*x as _, *y as _);
