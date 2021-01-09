4545fd3d4778bcfe3b8deed92bf81d5f6d8ff35a
JackRedstonia <jackredstonia64@gmail.com>
2021-01-09 23:44:30 +0700

    Attempt at audio player on GStreamer

--- src/bin/stacks/main.rs
+++ src/bin/stacks/main.rs
@@ -6,7 +6,7 @@ use stacks::framework::{
     widgets::{
         layout::{ContainerSize, VContainer},
         shapes::{Rect, Throbber},
-        Font, FontStyle, Parallax, Text, Transform,
+        AudioPlayer, Parallax, Transform,
     },
     Framework,
 };
@@ -21,20 +21,21 @@ fn main() {
                 vec![
                     Rect::new(
                         LayoutSize::min(200.0, 100.0).expand_width().expand_height(),
-                        Paint::new(Color4f::new(0.2, 0.8, 0.0, 0.3), None),
+                        Paint::new(Color4f::new(0.2, 0.8, 0.0, 0.3), None).with_anti_alias(true),
                         false,
                     )
                     .boxed(),
                     Rect::new(
                         LayoutSize::min(100.0, 100.0).expand_height_by(3.0),
-                        Paint::new(Color4f::new(0.7, 0.1, 0.2, 0.3), None),
+                        Paint::new(Color4f::new(0.7, 0.1, 0.2, 0.3), None).with_anti_alias(true),
                         false,
                     )
                     .boxed(),
                     Transform::new(
                         Rect::new(
                             LayoutSize::min(50.0, 100.0),
-                            Paint::new(Color4f::new(0.0, 0.0, 1.0, 1.0), None),
+                            Paint::new(Color4f::new(0.0, 0.0, 1.0, 1.0), None)
+                                .with_anti_alias(true),
                             false,
                         ),
                         Matrix::translate((20.0, 20.0)),
@@ -43,25 +44,19 @@ fn main() {
                     Throbber::new(
                         LayoutDimension::min(100.0),
                         {
-                            let mut p = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None);
+                            let mut p = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None)
+                                .with_anti_alias(true);
                             p.set_stroke_width(12.0);
-                            p.set_anti_alias(true);
                             p.set_stroke(true);
                             p
                         },
                         false,
                     )
                     .boxed(),
-                    Text::new(
+                    AudioPlayer::new(
                         LayoutSize::min(500.0, 100.0).expand_width(),
-                        "lorem ipsum dolor sit amet. いろはにほへと",
-                        Font::Default,
-                        FontStyle::Bold,
-                        {
-                            let mut p = Paint::new(Color4f::new(1.0, 1.0, 1.0, 1.0), None);
-                            p.set_anti_alias(true);
-                            p
-                        },
+                        Paint::new(Color4f::new(0.8, 0.8, 1.0, 1.0), None).with_anti_alias(true),
+                        Paint::new(Color4f::new(0.7, 0.7, 0.9, 0.7), None).with_anti_alias(true),
                     )
                     .boxed(),
                 ],
