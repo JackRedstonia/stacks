901ee162bccf264105affbbe6e4283c2e338e531
JackRedstonia <jackredstonia64@gmail.com>
2021-01-14 21:52:17 +0700

    cargo fmt

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ fn main() {
     // May the gods of crustaceans (and Rustaceans) bless Linus Torvalds and
     // Graydon Hoare that I only have to deal with 0.1% of Windows'
     // never-ending bullcrap.
-    
+
     let target = env::var("TARGET").unwrap();
     if target.contains("pc-windows") {
         let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
@@ -27,7 +27,7 @@ fn main() {
         lib_dir.push("64");
         dll_dir.push("64");
         println!("cargo:rustc-link-search=all={}", lib_dir.display());
-        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir") {
             let entry_path = entry.expect("Invalid fs entry").path();
             let file_name_result = entry_path.file_name();
             let mut new_file_path = manifest_dir.clone();
@@ -35,7 +35,8 @@ fn main() {
                 let file_name = file_name.to_str().unwrap();
                 if file_name.ends_with(".dll") {
                     new_file_path.push(file_name);
-                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+                    std::fs::copy(&entry_path, new_file_path.as_path())
+                        .expect("Can't copy from DLL dir");
                 }
             }
         }
