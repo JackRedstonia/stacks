e3ca4650f276bd6a9ed07a39cd097eadfac93cd8
JackRedstonia <jackredstonia64@gmail.com>
2021-01-23 02:47:12 +0700

    Add resource stack to framework

--- src/framework.rs
+++ src/framework.rs
@@ -6,6 +6,7 @@ use std::cell::RefCell;
 
 use crate::prelude::*;
 use game::{Error, Game, InputEvent, State, ID};
+use resource::ResourceStack;
 use skia::{Canvas, Size};
 use widgets::{LayoutSize, Widget, Wrap};
 
@@ -13,6 +14,8 @@ pub struct Framework<T: Widget> {
     root: Wrap<T>,
     layout_size: LayoutSize,
     size: Size,
+    
+    recycled_resource_stack: ResourceStack,
 }
 
 impl<T: Widget> Framework<T> {
@@ -20,12 +23,14 @@ impl<T: Widget> Framework<T> {
         let state = FrameworkState {
             current_focused_id: None,
             just_grabbed_focus: false,
+            load_requested: false,
         };
         FrameworkState::STATE.with(|x| *x.borrow_mut() = Some(state));
         Self {
             root: root.into(),
             layout_size: LayoutSize::ZERO,
             size: Size::new_empty(),
+            recycled_resource_stack: ResourceStack::new(),
         }
     }
 
@@ -43,11 +48,19 @@ impl<T: Widget> Framework<T> {
             self.root.input(&event);
         }
     }
+
+    fn maybe_load(&mut self) {
+        if FrameworkState::consume_load_request() {
+            self.root.load(&mut self.recycled_resource_stack);
+            assert!(self.recycled_resource_stack.is_empty());
+        }
+    }
 }
 
 impl<T: Widget> Game for Framework<T> {
     fn update(&mut self) {
         self.root.update();
+        self.maybe_load();
     }
 
     fn draw(&mut self, canvas: &mut Canvas) {
@@ -63,6 +76,7 @@ impl<T: Widget> Game for Framework<T> {
 
         // Do the actual drawing
         self.root.draw(canvas);
+        self.maybe_load();
     }
 
     fn set_size(&mut self, window_size: Size) {
@@ -71,10 +85,12 @@ impl<T: Widget> Game for Framework<T> {
             self.layout_size.height.min.max(window_size.height),
         );
         self.root.set_size(self.size);
+        self.maybe_load();
     }
 
     fn input(&mut self, event: InputEvent) {
         self.focus_aware_input(event);
+        self.maybe_load();
     }
 
     fn close(&mut self) {}
@@ -87,12 +103,25 @@ impl<T: Widget> Game for Framework<T> {
 pub struct FrameworkState {
     current_focused_id: Option<ID>,
     just_grabbed_focus: bool,
+    load_requested: bool,
 }
 
 impl FrameworkState {
     const PANIC_MESSAGE: &'static str =
         "Attempt to get framework state while framework is uninitialised";
     thread_local!(static STATE: RefCell<Option<FrameworkState>> = RefCell::new(None));
+
+    pub fn request_load() {
+        Self::with_mut(|x| x.load_requested = true);
+    }
+
+    fn consume_load_request() -> bool {
+        Self::with_mut(|x| {
+            let b = x.load_requested;
+            x.load_requested = false;
+            b
+        })
+    }
 
     pub fn current_focus() -> Option<ID> {
         Self::with(|x| x.current_focused_id)
