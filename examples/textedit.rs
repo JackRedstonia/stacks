54254129949f2b6d4eba120d5a44f00be23197ab
JackRedstonia <jackredstonia64@gmail.com>
2021-05-13 16:58:00 +0700

    big...

--- examples/textedit.rs
+++ examples/textedit.rs
@@ -13,7 +13,7 @@ fn main() {
     Framework::run("Stacks", || {
         let root = TextEdit::new(
             LayoutDimension::ZERO.expand(),
-            None,
+            Some(48.0),
             Paint::new_color4f(1.0, 1.0, 1.0, 1.0).anti_alias(),
         );
 
